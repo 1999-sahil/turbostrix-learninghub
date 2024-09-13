d2685e7ac8ddfb1c4fb0af2ba7d2162fcf08d512
Sahil <sahilahmed466@gmail.com>
2024-09-13 17:53:40 +0530

    Setting up Nextra for MDX Doc

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+const themeConfig = {
+    logo: <span style={{ color: 'red' }} className="font-semibold text-pink-500">TurboStrix</span>,
+    project: {
+      link: 'https://github.com/shuding/nextra'
+    }
+    // ... other theme options
+}
+
+export default themeConfig;
