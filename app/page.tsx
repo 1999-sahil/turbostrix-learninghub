8f24c36ec6029aa0f13540fd74044e26961f4299
Sahil <sahilahmed466@gmail.com>
2024-09-13 18:44:16 +0530

    feat: Adding theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col space-y-6 ml-4">
       <h2>home page</h2>
       <Link href="/frameworks/angular">
         doc1
@@ -10,6 +11,7 @@ export default function Home() {
       <Link href="/fruits/apple">
         doc2
       </Link>
+      <ModeToggle />
     </div>
   );
 }
